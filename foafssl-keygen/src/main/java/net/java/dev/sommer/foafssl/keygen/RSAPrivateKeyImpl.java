c81526fde2b22cc067b3b2d12ea1f3cbfb4e9007
Henry J. Story <henry.story@bblfish.net>
2010-03-22 18:20:30 +0100

    The test pass now. Added new foafssl-keygen to place some classes that kept re-appearing. Fixed modules to work with refactoring.

--- foafssl-keygen/src/main/java/net/java/dev/sommer/foafssl/keygen/RSAPrivateKeyImpl.java
+++ foafssl-keygen/src/main/java/net/java/dev/sommer/foafssl/keygen/RSAPrivateKeyImpl.java
@@ -1,46 +1,46 @@
 /*
-  * Copyright 2003-2008 Sun Microsystems, Inc.  All Rights Reserved.
-  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
-  *
-  * This code is free software; you can redistribute it and/or modify it
-  * under the terms of the GNU General Public License version 2 only, as
-  * published by the Free Software Foundation.  Sun designates this
-  * particular file as subject to the "Classpath" exception as provided
-  * by Sun in the LICENSE file that accompanied this code.
-  *
-  * This code is distributed in the hope that it will be useful, but WITHOUT
-  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-  * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
-  * version 2 for more details (a copy is included in the LICENSE file that
-  * accompanied this code).
-  *
-  * You should have received a copy of the GNU General Public License version
-  * 2 along with this work; if not, write to the Free Software Foundation,
-  * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
-  *
-  * Please contact Sun Microsystems, Inc., 4150 Network Circle, Santa Clara,
-  * CA 95054 USA or visit www.sun.com if you need additional information or
-  * have any questions.
-  */
+ * New BSD license: http://opensource.org/licenses/bsd-license.php
+ *
+ * Copyright (c) 2010
+ * Henry Story
+ * http://bblfish.net/
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * - Redistributions of source code must retain the above copyright notice,
+ *  this list of conditions and the following disclaimer.
+ * - Redistributions in binary form must reproduce the above copyright notice,
+ *  this list of conditions and the following disclaimer in the documentation
+ *  and/or other materials provided with the distribution.
+ * - Neither the name of bblfish.net nor the names of its contributors
+ *  may be used to endorse or promote products derived from this software
+ *  without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
 
 
-package net.java.dev.sommer.foafssl.verifier;
+package net.java.dev.sommer.foafssl.keygen;
 
-import java.math.BigInteger;
-
-
+import sun.security.pkcs.PKCS8Key;
+import sun.security.util.DerOutputStream;
+import sun.security.util.DerValue;
 
 import java.io.IOException;
 import java.math.BigInteger;
-
 import java.security.InvalidKeyException;
 import java.security.interfaces.RSAPrivateKey;
-
-import sun.security.pkcs.PKCS8Key;
-import sun.security.rsa.RSAKeyFactory;
-import sun.security.rsa.RSAPrivateCrtKeyImpl;
-import sun.security.util.DerOutputStream;
-import sun.security.util.DerValue;
 
 /**
  * Key implementation for RSA private keys, non-CRT form (modulus, private
@@ -51,8 +51,8 @@ import sun.security.util.DerValue;
  *
  * Hacked for tests by Henry Story. Don't use this in production.
  *
- * @see RSAPrivateCrtKeyImpl
- * @see RSAKeyFactory
+ * @see sun.security.rsa.RSAPrivateCrtKeyImpl
+ * @see sun.security.rsa.RSAKeyFactory
  *
  * @since   1.5
  * @author  Andreas Sterbenz
@@ -114,4 +114,3 @@ public final class RSAPrivateKeyImpl extends PKCS8Key implements RSAPrivateKey {
     }
 
 }
-
